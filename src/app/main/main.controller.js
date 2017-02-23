b40f4db6650d0f96b8b838f354ab741a153d0043
HenryGBC <henry.bravo89@gmail.com>
2017-02-23 11:58:47 -0430

    Usuarios Logic

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -10,7 +10,7 @@
     var vm = this;
     angular.extend(vm, {
       users: [],
-      userName: '',
+      user: {name: '', win:0},
       turnPlayerOne: true,
       nameUser: "Jugador n° 1",
       setUser: setUser,
@@ -21,17 +21,20 @@
     });
     activate();
 
-    function setUser() {
-        if(!vm.userName){
+    function setUser(user) {
+        if(!vm.user.name){
           vm.errorInput = true;
           return ;
         }else{
            vm.errorInput = false;
-           _addUser();
+           _addUser(user);
         }
     }
     function changeValue(row, column){
-      vm.arrayBoard[row][column] = 2;
+      vm.arrayBoard[row][column] = vm.turnPlayerOne ? 1:2; 
+      vm.turnPlayerOne = !vm.turnPlayerOne;
+       var turn = vm.turnPlayerOne ? 1:2; 
+      _sheck(turn, vm.arrayBoard, row, column, 0);
     }
     function activate() {
       _initArray();
@@ -41,30 +44,50 @@
 
     // Init Array for tableboard
     function _initArray(){
+     
        vm.arrayBoard= new Array(new Array(0,0,0), new Array(0,0,0), new Array(0,0,0));
     }
 
     //Set Name Player One and Two
-    function _addUser(){
+    function _addUser(user){
       vm.users = MainService.getUsers();
-      console.log(vm.users);
-      console.log(vm.users.length);
       if(vm.users.length < 3){
-         MainService.setUser(vm.userName);
-         vm.userName='';
+        console.log(user);
+         var userName = {
+          name: user,
+          win: 0
+         };
+         MainService.setUser(userName);
+         vm.user.name='';
          vm.nameUser= "Jugador n° 2";
       }
 
       if(vm.users.length === 2){
-        _play();
+        _initPlay();
       }
     }
 
     //Init play tictactoe
-    function _play(){
+    function _initPlay(){
       vm.play = true;
       console.log(vm.users);
       console.log(vm.users.length);
+    }
+
+    function _sheck(turn, array, row, column, cont){
+      if(row >= 0 && row <=3){
+        console.log(array[row][column+1]);
+         if(array[row][column+1]==turn){
+              console.log(cont);
+            _sheck(turn, array, row+1, column, cont+1);
+         }
+        
+      }
+       if(array[row][column+1]==turn){
+              console.log(cont);
+            _sheck(turn, array, row+1, column, cont+1);
+         }
+      
     }
   }
 
